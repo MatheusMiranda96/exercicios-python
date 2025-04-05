58439bcd4aa9ad2f4db423413a8cb29236dc853b
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-05 13:00:17 -0300

    Update Desafio 01

--- desafios/desafio01.py
+++ desafios/desafio01.py
@@ -2,4 +2,4 @@
 # Fazer um input que pergunte o nome do usuário, e retorne uma mensagem de boas vindas.
 
 nome = input("Qual é seu nome? ")
-print("Olá,", nome+".", ("Prazer em te conhecer!"))
+print("É um prazer te conhecer, {}!".format(nome))
