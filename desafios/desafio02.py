d5d6aef65144bd45358ad584a2db9cfcac646951
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-05 11:06:03 -0300

    Desafio 02 - Curso em video

---
+++ desafios/desafio02.py
@@ -0,0 +1,7 @@
+# Desafio do curso em video.
+# Fazer um input que pergunte o dia, mês e ano que o usuário nasceu, e retorne uma mensagem formatada.
+
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano+'.')
