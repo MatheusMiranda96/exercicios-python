65fd4822a7b0d1f627c5ef36461bd01d4432ebff
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-05 11:32:35 -0300

    Desafio 03 - Curso em video

---
+++ desafio03.py
@@ -0,0 +1,8 @@
+# Desafio do curso em video.
+# Fazer um script que some dois valores inteiros digitados pelo usuário.
+
+n1 = int(input("Digite N1: "))
+n2 = int(input("Digite N2: "))
+soma = n1 + n2
+
+print("A soma dos valores é: ", soma)
