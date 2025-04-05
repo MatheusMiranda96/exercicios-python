fa8d0f28c8dbc7c26736c92a8cf33138bc31ebd3
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-05 10:54:18 -0300

    desafio 01

---
+++ desafios/desafio01.py
@@ -0,0 +1,5 @@
+# Desafio do curso em video.
+# Fazer um input que pergunte o nome do usuário, e retorne uma mensagem de boas vindas.
+
+nome = input("Qual é seu nome? ")
+print("Olá,", nome+".", ("Prazer em te conhecer!"))
