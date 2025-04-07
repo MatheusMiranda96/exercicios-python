47acf33b88ed705574dbf30d75328aa01fc83784
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-07 12:21:49 -0300

    praticando

---
+++ praticas/verificador_idade.py
@@ -0,0 +1,9 @@
+print("=" * 10, "Verificador de idade", "=" * 10)
+idade = int(input("Informe sua idade: "))
+
+if idade >= 18:
+    print("Entrada permitida!")
+else:
+    print("Acesso negado!")
+
+print("=" * 42)
