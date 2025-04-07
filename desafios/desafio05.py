25a23ccbf6527c133f2ac8d8338ab04eb23c48fb
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-07 12:38:16 -0300

    Desafio 05 - Curso em video

---
+++ desafios/desafio05.py
@@ -0,0 +1,8 @@
+# Desafio do curso em video
+# Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
+
+n = int(input("Digite um número inteiro: "))
+s = n + 1
+a = n - 1
+print(f"Sucessor: {s}")
+print(f"Antecessor: {a}")
