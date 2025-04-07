eb37e56c4e81ed72440db45223ae53d8df614430
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-07 16:19:55 -0300

    Desafio 08 - Curso em video

---
+++ desafios/desafio08.py
@@ -0,0 +1,9 @@
+# Desafio do curso em video
+# Escreva um programa que leia o valor em metros e o exiba convertido em centimetros e milimetros.
+
+m = float(input("Digite o valor em metros: "))
+c = m * 100
+mm = m * 1000
+
+print(f"Centimentos: {c}")
+print(f"Milimetros: {mm}")
