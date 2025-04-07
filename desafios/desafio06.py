21ef0eca0a6d16fef3beb8b121861b8bf823d2df
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-07 12:54:36 -0300

    Desafio 06 - Curso em video

---
+++ desafios/desafio06.py
@@ -0,0 +1,12 @@
+# Desafio do curso em video
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input("Digite um valor: "))
+
+d = n * 2 #Dobro
+t = n * 3 #Triplo
+r = n ** (1/2) #Raiz quadrada
+
+print(f"Dobro: {d}")
+print(f"Triplo: {t}")
+print(f"Raiz quadrada: {r}")
