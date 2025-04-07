8c66a4645948df8ad5282ac8f1cb1b10ad86f0b7
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-07 16:54:46 -0300

    Desafio 10 - Curso em video

---
+++ desafios/desafio10.py
@@ -0,0 +1,8 @@
+# Desafio do curso em video
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+
+din = float(input("Valor na carteira: "))
+dol = din / 5.91
+
+print(f"Você pode comprar: {dol:.2f} dólares.")
+
