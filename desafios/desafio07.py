da84d009d70c53503c6b294a8ace201495c078e4
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-07 13:01:37 -0300

    Desafio 07 - Curso em video

---
+++ desafios/desafio07.py
@@ -0,0 +1,9 @@
+# Desafio do curso em video
+# Desenvolva um programa que leia duas notas de um aluno, calcule e mostre sua média.
+
+n1 = float(input("Digite a primeira nota: "))
+n2 = float(input("Digite a segunda nota: "))
+m = (n1 + n2)/2
+
+print(f"Média do aluno: {m}")
+
