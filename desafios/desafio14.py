d3d8fba83be1da1b2c424db9869cf0ff07fef2eb
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-08 11:18:43 -0300

    Desafio 14 - Curso em video

---
+++ desafios/desafio14.py
@@ -0,0 +1,7 @@
+# Desafio do curso em video
+# Conversor de Temperaturas
+
+c = float(input("Informe a temperatura em ºC: "))
+f = ((9 * c) / 5) + 32  # Da para usar sem (). Ordem de precedência
+
+print(f"A temperatura de {c}ºC corresponde a {f}ºF")
