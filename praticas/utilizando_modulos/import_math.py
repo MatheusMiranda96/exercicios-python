1fdeea27f353ef60b9b293a657f79172490dcc82
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-08 11:02:38 -0300

    Utilizando modulos

---
+++ praticas/utilizando_modulos/import_math.py
@@ -0,0 +1,7 @@
+import math            # importa toda a biblioteca
+from math import sqrt  # importa apenas a respectiva funcionalidade
+
+num = int(input("Digite um número: "))
+raiz = sqrt(num)
+
+print(f"A raiz quadrada de {num} é {raiz:.2f}")
