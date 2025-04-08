7b09f3b599dc06da389a67d4067bacdadabe6bcf
matheusmiranda96 <matth.miranda@gmail.com>
2025-04-08 11:51:20 -0300

    Desafio 16 - Curso em video

---
+++ desafios/desafio16.py
@@ -0,0 +1,9 @@
+# Desafio do curso em video
+# Crie um programa que leia um número real e mostre sua porção inteira.
+
+import math
+
+n = float(input("Digite um número: "))
+pi = math.floor(n) # floor mostra parte inteira do valor.
+
+print (f"Parte inteira do número: {pi}")
